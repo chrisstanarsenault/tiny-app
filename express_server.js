26cfcc551bbfed6576acd33de0d7815dd834a519
Chris Arsenault <chrisstanarsenault@gmail.com>
2018-11-04 21:35:39 +0000

    finished the basic skeleton of the app.  Still needs to be prettied up, but everything works as intended.
    updated comments, and removed some unused code, and updated and added some new links on most pages to make navigation easier.

--- express_server.js
+++ express_server.js
@@ -8,8 +8,8 @@ const bcrypt = require('bcrypt')
 app.use(bodyParser.urlencoded({entended: true}));
 app.use(cookieSession( {
   name: 'session',
-  keys: ["purple-monkey-dishwasher", "quacker-the-hacker"],
-  maxAge: 24 * 60 * 60 * 1000
+  keys: ["purple-monkey-dishwasher"],
+  maxAge: 10 * 60 * 1000 //expires after 10 minutes
 }))
 
 app.set("view engine", "ejs");
@@ -20,10 +20,8 @@ function generateRandomString() {
   for (let i = 0; i < 6; i++) {
     randomString += possibleLettersAndNumbers.charAt(Math.floor(Math.random() * possibleLettersAndNumbers.length));
   }
-
   return randomString;
 }
-
 
 let urlDatabase = {
   "b2xVn2": {
@@ -49,13 +47,14 @@ const users = {
   }
 }
 
-const userLoggedOn = (req) => req.session.user_ID; //thanks to Sylvain Junca for the tip for this great helper function
+// function to check session to see if a user is already logged on.  Thanks to Sylvain Junca for the tip for this great helper function
+const userLoggedOn = (req) => req.session.user_ID;
+//function to check if item belongs to the logged in user.
 const userIsOwner = (req) => {
   return (req.session.user_ID === urlDatabase[req.params.id]["userID"])
 };
 
-
-
+// creates object storing urls belonging to logged in user.
 const urlsForUser = (id) => {
   const userURL = {}
   for (each in urlDatabase) {
@@ -66,45 +65,50 @@ const urlsForUser = (id) => {
   return userURL;
 }
 
-
-
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  if (userLoggedOn(req)) {
+    res.redirect("/urls")
+  } else {
+    res.redirect("/login")
+  }
 });
 
 app.get("/urls", (req, res) => {
   if (userLoggedOn(req)) {
-
     let templateVars = {
       urls: urlsForUser(req.session.user_ID),
+      user: users[req.session.user_ID],
       user_ID: req.session.user_ID
     }
     res.render("urls_index", templateVars);
   } else {
-    res.send('You need to log in or register');
+    res.send(`Time to <a href='/login'>log in </a> or <a href='/register'>register</a>`);
   }
 });
 
 app.get("/urls/new", (req, res) => {
   if (userLoggedOn(req)) {
   let templateVars = {
+    user: users[req.session.user_ID],
     urls: urlDatabase,
     user_ID: req.session.user_ID
   }
   res.render("urls_new", templateVars);
-
  } else {
     res.redirect("/login")
   }
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body.longURL); // debug statement to see POST parameters
+  if (userLoggedOn(req)){
   let shortString = generateRandomString()
   urlDatabase[shortString] = {
     url:req.body["longURL"],
     userID: req.session.user_ID}
   res.redirect(`/urls/${shortString}`);
+  } else {
+    res.send('Make sure to log in if you want to do some URL shortening!')
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -113,7 +117,8 @@ app.get("/urls/:id", (req, res) => {
   } if (userIsOwner(req)) {
   let templateVars = {
     shortURL: req.params.id,
-    urls: urlDatabase,
+    urls: urlDatabase[req.params.id],
+    user: users[req.session.user_ID],
     user_ID: req.session.user_ID
   }
   res.render("urls_show", templateVars);
@@ -123,9 +128,15 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  if (userIsOwner(req)) {
-  urlDatabase[req.params.id]["url"] = req.body[req.params.id];
-  res.redirect('/urls');
+  if (userLoggedOn(req)) {
+    if (userIsOwner(req)) {
+    urlDatabase[req.params.id]["url"] = req.body[req.params.id];
+    res.redirect('/urls');
+    } else {
+      res.send("Turn away!  You don't belong here!  Login or register!")
+    }
+  } else {
+    res.send(`Why don't you try <a href="/login">logging in</a> or <a href="/register">registering</a>?`)
   }
 });
 
@@ -137,7 +148,11 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-   let templateVars = {
+  if (userLoggedOn(req)) {
+    res.redirect('/urls')
+  }
+
+  let templateVars = {
      user_ID: users[req.session.user_ID]
    }
   res.render("user-login", templateVars)
@@ -152,18 +167,23 @@ app.post("/login", (req, res) => {
         return
       }
     }
-    res.status(403).send()
+    res.status(403).send(`Oops, either your email or password was incorrect!  Try <a href="/login">logging in</a> again!`)
   }
 });
 
 app.post("/logout", (req, res) => {
   delete req.session.user_ID;
-  //req.session = null
   res.redirect('/urls');
 });
 
 app.get("/register", (req, res) => {
-  res.render('user-register')
+  if (userLoggedOn(req)) {
+    res.redirect('/urls')
+  }
+  let templateVars = {
+    user: users[req.session.user_ID]
+  }
+  res.render('user-register', templateVars)
 })
 
 app.post("/register", (req, res) => {
@@ -176,41 +196,44 @@ app.post("/register", (req, res) => {
   if(email && password) {
     for (user in users) {
       if (users[user]["email"] === email) {
-      emailDoesExist = true;
+        emailDoesExist = true;
+      }
     }
-  }
 
-  if (!emailDoesExist) {
-    let randomUserID = generateRandomString()
-    req.session.user_ID = randomUserID
-    users[randomUserID] = {
-      id: randomUserID,
-      email: email,
-      password: hashedPassword
-    };
+    if (!emailDoesExist) {
+      let randomUserID = generateRandomString()
+      req.session.user_ID = randomUserID;
+      users[randomUserID] = {
+        id: randomUserID,
+        email: email,
+        password: hashedPassword
+      };
     res.redirect("/urls")
+  } else {
+    res.status(400).send(`Your email already exists! Try <a href="/register"> registering </a> again with a different email!`)
     }
-
-  if (emailDoesExist) {
-      res.status(400).send("Error 400 - Your email already exisits!")
-    } else {
-      res.status(400).send("Error 400 - Fill out all forms properly!")
-    }
+  } else {
+    res.status(400).send(`Fill out all forms properly!  Try <a href="/register"> registering </a> again!`)
   }
+  // if (emailDoesExist) {
+  //     res.status(400).send(`Your email already exists! Try <a href="/register"> registering </a> again with a different email!`)
+  //   } else {
+  //     res.status(400).send(`Fill out all forms properly!  Try <a href="/register"> registering </a> again!`)
+  //   }
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
+  let longURL = urlDatabase[req.params.shortURL]['url'];
   res.redirect(longURL);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><bod>Hello <b>World</></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><bod>Hello <b>World</></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
